6f10e37f9df1ffc557d7808e6a276fc94ca0d096
tiagosestari <tiagosestari@gmail.com>
2020-12-18 21:01:48 -0300

    Componente Resumo principal puxa o valor emprestado e o valor pago

--- desafio-dash-tiagosestari/src/components/Dash/index.js
+++ desafio-dash-tiagosestari/src/components/Dash/index.js
@@ -2,6 +2,10 @@ import React, {useEffect} from 'react';
 import {useState} from 'react';
 import axios from 'axios';
 
+//Importando outros componentes
+import AmountTaken from '../ResumoPrincipal';
+import ResumoPrincipal from '../ResumoPrincipal';
+
 const Dash = () => {
     const baseurl = 'http://www.mocky.io/v2/5c923b0932000029056bce39'
     const [findata, setFindata] = useState({});
@@ -17,10 +21,14 @@ const Dash = () => {
         
     }, [])
 
+    console.log(findata)
+
     
   return (
    <div>
-       
+
+    <ResumoPrincipal amountTaken={findata.amountTaken} amountPaid={findata.amountPayd} />
+
     
    
    </div>
