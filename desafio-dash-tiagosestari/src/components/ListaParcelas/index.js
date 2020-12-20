e6e353109b1312c7a3de17344da2bf58a02aeb7c
tiagosestari <tiagosestari@gmail.com>
2020-12-20 18:32:18 -0300

    Implementado filtro de parcelas pendentes e pagas

--- desafio-dash-tiagosestari/src/components/ListaParcelas/index.js
+++ desafio-dash-tiagosestari/src/components/ListaParcelas/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './ListaParcelas.css';
-
+import {useState} from 'react';
 
 const ListaParcelas = (props) => {
-    
     //Os dados financeiros serão recebidos como props
     const {installments} = props;
 
+    const [filtroPagas, setFiltroPagas] = useState('Todas');
+    const [filteredInstallments, setFilteredInstallments] = useState(installments)
+
+    //Filtrando a lista toda vez que o estado filtro lista for alterado
+    useEffect(() => {
+      if(filtroPagas === 'Pagas') {
+        let novaLista = installments.filter((installment) => {return (installment.payd)})     
+        setFilteredInstallments(novaLista)   
+      } else if(filtroPagas === 'Em aberto') {
+        let novaLista = installments.filter((installment) => {return (!installment.payd)})     
+        setFilteredInstallments(novaLista)
+      } else {
+        let novaLista = installments.filter((installment) => {return (true)})     
+        setFilteredInstallments(novaLista)
+      }
+    },[filtroPagas])
+    
+
     //Mapeando o array de parcelas
-    const listaDeParcelas = installments.map((installment) => {
+    const listaDeParcelas = filteredInstallments.map((installment) => {
 
         return (
             <div className="listItem">
@@ -27,6 +44,14 @@ const ListaParcelas = (props) => {
         <div>Status </div> 
         <div>Venc. </div>
         <div>Pagar </div>
+    </div>
+
+    <div>
+      <select value={filtroPagas} onChange={(event) => {setFiltroPagas(event.target.value); console.log(filtroPagas)}}>
+        <option>Pagas</option>
+        <option>Em aberto</option>
+        <option>Todas</option>
+      </select>
     </div>
 
     <div className="listContainer"> {listaDeParcelas} </div>
